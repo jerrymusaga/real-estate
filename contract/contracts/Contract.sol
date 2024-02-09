49cd227723b5df39841ed72ee2cda43ef2c26ea1
jerrymusaga <musagatechnology@gmail.com>
2024-02-09 11:53:35 +0100

    review and product layout

--- contract/contracts/Contract.sol
+++ contract/contracts/Contract.sol
@@ -17,6 +17,37 @@ contract MyContract {
 
     }
 
+    mapping(uint => Property) private properties;
+    uint public propertyIndex;
+
+    event PropertyListed(uint indexed id, address indexed owner, uint price);  
+    event PropertySold(uint indexed id, address indexed oldOwner, uint indexed newOwner, uint price);
+    event PropertyReSold(uint indexed id, address indexed oldOwner, uint indexed newOwner, uint price);
+
+    struct Review{
+        uint productId;
+        address reviewer;
+        uint rating;
+        string comment;
+        uint likes;
+    }
+
+    mapping(uint => Review) private reviews;
+    mapping(address => uint[]) private userReviews;
+    mapping(uint => Product) private products;
+
+    uint public reviewsCounter;
+
+    event ReviewAdded(uint indexed productId, address indexed reviewer, uint indexed rating, string comment );
+    event ReviewLiked(uint indexed productId, uint indexed reviewIndex, address indexed liker, uint indexed likes);
+    
+
+    struct Product{
+        uint productId;
+        uint totalRating;
+        uint numOfReviews;
+    }
+
     constructor() {}
 
     function listProperty() external returns (uint){
